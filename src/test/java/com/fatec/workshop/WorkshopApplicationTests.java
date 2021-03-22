e1c1b69126e4cdf37d5ac9db3b97653c0c71ac8c
Matheus Ribeiro <matheus.ribs.sjc@gmail.com>
2021-03-22 18:32:01 -0300

    Projeto criado

---
+++ src/test/java/com/fatec/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
