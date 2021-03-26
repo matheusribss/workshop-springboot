1389c1fc7bb2d62b8a51ce2a3a943ac955a05d2f
Matheus Ribeiro <matheus.ribs.sjc@gmail.com>
2021-03-26 17:31:02 -0300

    Exception handling -findById

--- src/main/java/com/fatec/workshop/services/UserService.java
+++ src/main/java/com/fatec/workshop/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.fatec.workshop.entities.User;
 import com.fatec.workshop.repositories.UserRepository;
+import com.fatec.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service //REGISTRA COMO COMPONONENTE DO SPRING PARA INJETADO AUTOMATICAMENTE PELO SPRING
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get(); 
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
